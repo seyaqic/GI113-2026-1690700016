85918066d54204610eab75c099784b2d463868e8
seyaqic <phcskpranpriya@gmail.com>
2026-08-24 11:20:00 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID : 1690700016
+ * Name       : Pranpriya Phetchuensakul
+ * Section    : 129A
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp}/{maxHp}");
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine($"===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP; {currentHp} / {maxHp}");
+            Console.WriteLine($"HP Percent: 47%");
         }
     }
 }
